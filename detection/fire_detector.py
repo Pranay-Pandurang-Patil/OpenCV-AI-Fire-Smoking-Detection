23e8c6a2a966d599ab8904fdaf44b0e01c958ef8
Pranay-Pandurang-Patil <pranaypatilknp@gmail.com>
2026-08-11 20:16:16 +0530

    Add automatic external model download

--- detection/fire_detector.py
+++ detection/fire_detector.py
@@ -1,5 +1,40 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
 from ultralytics import YOLO
+
 from config import FIRE_MODEL_PATH, CONFIDENCE
+
+
+MODEL_URL = (
+    "https://raw.githubusercontent.com/"
+    "luminous0219/fire-and-smoke-detection-yolov8/"
+    "main/weights/best.pt"
+)
+
+
+def download_model():
+
+    model_path = Path(FIRE_MODEL_PATH)
+
+    if not model_path.exists():
+
+        model_path.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+        print("Downloading fire detection model...")
+
+        urlretrieve(
+            MODEL_URL,
+            model_path
+        )
+
+        print("Fire detection model downloaded.")
+
+
+download_model()
 
 model = YOLO(FIRE_MODEL_PATH)
 
--- detection/smoking_detector.py
+++ detection/smoking_detector.py
@@ -1,7 +1,40 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
 from ultralytics import YOLO
 
 from config import SMOKING_MODEL_PATH, CONFIDENCE
 
+
+MODEL_URL = (
+    "https://raw.githubusercontent.com/"
+    "alihassanml/Smoking-detection-yolo11/"
+    "main/best.onnx"
+)
+
+
+def download_model():
+
+    model_path = Path(SMOKING_MODEL_PATH)
+
+    if not model_path.exists():
+
+        model_path.parent.mkdir(
+            parents=True,
+            exist_ok=True
+        )
+
+        print("Downloading smoking detection model...")
+
+        urlretrieve(
+            MODEL_URL,
+            model_path
+        )
+
+        print("Smoking detection model downloaded.")
+
+
+download_model()
 
 model = YOLO(
     SMOKING_MODEL_PATH,
